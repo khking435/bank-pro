7ca61ef70dec509dcaf9460e1d248ea5dd1e9242
+ <+>
2024-05-13 18:10:29 +0300

    cope

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,15 +1,14 @@
-import React from 'react'; // Importing React library
-import './SearchBar.css'; // Importing CSS file for styling
+import React from 'react';
+import './SearchBar.css'; 
 
-// Defining SearchBar component as a functional component
 const SearchBar = ({ searchTerm, setSearchTerm }) => {
-  // Function to handle input change
+
   const handleInputChange = (event) => {
-    // Update the searchTerm state with the new value entered by the user
+  
     setSearchTerm(event.target.value);
   };
 
-  // Rendering the SearchBar component
+
   return (
     <div className="search-bar">
       
